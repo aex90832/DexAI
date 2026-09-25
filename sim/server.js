5b7c42e5206e4c846718590f047733ad2056f482
aex90832 <aex90832@gmail.com>
2026-09-24 20:30:10 -0500

    Update server.js

--- sim/server.js
+++ sim/server.js
@@ -1,3 +1,29 @@
+/*
+ * DexAI - An Open Source AI Tool for Pokémon Analysis
+ * Copyright (C) 2026 @aex90832 (https://github.com/aex90832
+ *
+ * Created with human authoring and AI assistance (self-hosted Qwen & Claude).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useState } from 'react';
+
+export function DexAIPanel() {
+  // Your code here
+}
+
 'use strict';
 
 const express = require('express');
